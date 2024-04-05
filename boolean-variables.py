58aa5681e1629647a94f4d377c841b8637267b3d
Aman07a <aman16.aa17@gmail.com>
2024-04-05 15:22:34 +0200

    Added boolean-variables

---
+++ boolean-variables.py
@@ -0,0 +1,11 @@
+# Creating a variable named my_baby_bool and setting it equal to the string "true"
+my_baby_bool = "true"
+
+# Checking the type of my_baby_bool
+print(type(my_baby_bool))
+
+# Creating a boolean variable named my_baby_bool_two and setting it equal to True
+my_baby_bool_two = True
+
+# Checking the type of my_baby_bool_two
+print(type(my_baby_bool_two))
