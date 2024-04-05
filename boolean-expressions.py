407d20153ad9c4f902c04f0978141d32e6bc8039
Aman07a <aman16.aa17@gmail.com>
2024-04-05 14:46:31 +0200

    Added boolean-expressions

---
+++ boolean-expressions.py
@@ -0,0 +1,9 @@
+example_statement = "No"  # 'My dog is named Pavel.' is not a boolean expression
+
+statement_one = "Yes"  # Dogs are mammals - Boolean expression
+
+statement_two = "Yes"   # My dog is named Pavel - Not a boolean expression
+
+statement_three = "No"  # Dogs make the best pets - Not a boolean expression
+
+statement_four = "Yes"  # Cats are female dogs - Boolean expression
