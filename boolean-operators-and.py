882622105cb850d94b4a627d06d8035c470a1a52
Aman07a <aman16.aa17@gmail.com>
2024-04-08 10:30:55 +0200

    Added boolean-operators-and

---
+++ boolean-operators-and.py
@@ -0,0 +1,15 @@
+# Checkpoint 1: Set the variables statement_one and statement_two equal to the results of the following boolean expressions:
+
+# Statement one:
+statement_one = (2 + 2 + 2 >= 6) and (-1 * -1 < 0)
+
+# Statement two:
+statement_two = (4 * 2 <= 8) and (7 - 1 == 6)
+
+# Checkpoint 2: Rewrite the if statement to check if a student meets both requirements using an and statement.
+
+credits = 120
+gpa = 3.4
+
+if credits >= 120 and gpa >= 2.0:
+    print("You meet the requirements to graduate!")
