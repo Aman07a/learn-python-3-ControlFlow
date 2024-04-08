5aa1c0281fe4087a369a5f464259c74a293d74e8
Aman07a <aman16.aa17@gmail.com>
2024-04-08 12:21:50 +0200

    Added else-if-statements

---
+++ else-if-statements.py
@@ -0,0 +1,14 @@
+# Define the grade variable
+grade = 86
+
+# Write an if/elif/else statement to determine the letter grade
+if grade >= 90:
+    print("A")
+elif grade >= 80:
+    print("B")
+elif grade >= 70:
+    print("C")
+elif grade >= 60:
+    print("D")
+else:
+    print("F")
