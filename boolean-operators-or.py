8b8279193a2a5fd452551413e7f587e2e7b8b00f
Aman07a <aman16.aa17@gmail.com>
2024-04-08 11:14:13 +0200

    Added boolean-operators-or

---
+++ boolean-operators-or.py
@@ -0,0 +1,14 @@
+# Set the variables statement_one and statement_two equal to the results of the following boolean expressions:
+
+# Statement one:
+statement_one = (2 - 1 > 3) or (-5 * 2 == -10)
+
+# Statement two:
+statement_two = (9 + 5 <= 15) or (7 != 4 + 3)
+
+# Write an if statement to check if a student either has 120 or more credits or a GPA 2.0 or higher.
+credits = 118
+gpa = 2.0
+
+if credits >= 120 or gpa >= 2.0:
+    print("You have met at least one of the requirements.")
