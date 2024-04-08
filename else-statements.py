c39035d510ce2b7e98e1063fab5145784558c58b
Aman07a <aman16.aa17@gmail.com>
2024-04-08 11:50:39 +0200

    Added else-statements

---
+++ else-statements.py
@@ -0,0 +1,9 @@
+# Previous if statement with an additional check and an else statement:
+
+credits = 120
+gpa = 1.9
+
+if credits >= 120 and gpa >= 2.0:
+    print("You meet the requirements to graduate!")
+else:
+    print("You do not meet the requirements to graduate.")
